Tidy up the log likelihood function. Remove a wayward negative sign applied to dynesty results that caused the value to be reported incorrectly. Update the bounds for scipy to ensure they are the same as the dynesty bounds. Update the scipy tolerances to be something that I determined seems to reproduce the same fits as dynesty, plus or minus a single point of log likelihood. Fix the value that gets filled in the fit_params dictionary so it has an appropriate label depending on what package was used. Update the plot_line_fit() routine to account for the new fit_params keys, correctly display the max log likelihood at all times with the correct sign, and compute the correct chisquared and reduced chisquared from the log likelihood.

maven_iuvs/echelle.py
@@ -2172,7 +2172,12 @@ def fit_flat_data(light_fits, spectrum, data_unc, bad_frames=None,
         # Now make into dictionaries, excluding the max log likelihood since 
         # it's not included in the parameter names
         fit_params_dict = make_fit_param_dict(fit_params[:-1])
-        fit_params_dict['maxLL'] = fit_params[-1] # Add max log likeilhood 
+
+        # Package used affects what was done. Get it right
+        if kwargs['fitter']=='scipy':
+            fit_params_dict['min_neg_LL'] = fit_params[-1]
+        elif kwargs['fitter']=='dynesty':
+            fit_params_dict['maxLL'] = fit_params[-1]
         fit_unc_dict = make_fit_param_dict(fit_1sigma, is_fitparams=False)
        
         if i in bad_frames:
@@ -2851,7 +2856,12 @@ def fit_H_and_D(pig, wavelengths, spec, light_fits, CLSF, unc=1,
     lineshape_model_args = (wavelengths, edges, CLSF, BU_bg, fit_IPH_component)
 
     # Now call the fitting routine
+    a = -1.0
+    b = 2.0
+    
     if fitter == "scipy":
+        x_tol = 1e-5
+        f_tol = 1e-8
         try:
             bestfit = sp.optimize.minimize(negloglikelihood_jit, pig,
                                            args=objfn_args,
@@ -2859,21 +2869,23 @@ def fit_H_and_D(pig, wavelengths, spec, light_fits, CLSF, unc=1,
                                            # for xtol and ftol, at least 1e-3 
                                            # is required for happiness; 
                                            # 1e-4 is default
-                                           options={"xtol":1e-5,  
-                                                   "ftol":1e-5},
-                                           bounds=[(None, None),  # DN_H
-                                                   (None, None),  # DN_D
-                                                   (None, None),  # DN_IPH
-                                                   (121.55, 121.58),  # λ H 
+                                           
+                                           options={"xtol": x_tol,  
+                                                   "ftol": f_tol},
+                                           bounds=[(pig[0]*a, pig[0]*b),  # DN_H
+                                                   (pig[1]*a, pig[1]*b),  # DN_D
+                                                   (-pig[2]/5, pig[2]*5),  # DN_IPH
+                                                   (pig[3]-0.02, pig[3]+0.02),  # λ H 
                                                    # IPH λ:
                                                    (pig[4]-IPH_wv_spread/2,
                                                    pig[4]+IPH_wv_spread/2),
                                                    # IPH width in nm:
                                                    (IPH_minw, IPH_maxw),
                                                    # Background terms:
-                                                   (None, None), # intercept
-                                                   (None, None), # linear term
-                                                   (None, None)] # quadratic
+                                                   (-1e5, 1e5), # intercept
+                                                   (-1e5, 1e5), # linear term
+                                                   (-1e5, 1e5) # quadratic
+                                                   ] 
                         )
 
         except ValueError as e:
@@ -3069,7 +3081,7 @@ def fit_H_and_D(pig, wavelengths, spec, light_fits, CLSF, unc=1,
 
         samples = dresults.samples
         weights = dresults.importance_weights()
-        max_logl = -max(dresults.logl)
+        max_logl = max(dresults.logl)
 
         modeled_params, covariance = dyfunc.mean_and_cov(samples, weights)
         fit_uncert = np.sqrt(np.diag(covariance))
@@ -3292,10 +3304,9 @@ def loglikelihood(params, wavelength_data, binedges, CLSF, data, uncertainty, BU
     N = len(DN_fit)
     # Fit the model to the existing data assuming Gaussian distributed photo events
 
-    # Old: -jnp.sum((data - DN_fit)**2 / (2*(uncertainty**2)))
-    # Corrected:
-    L = -(N/2)*jnp.log(2*math.pi) - jnp.sum(jnp.log(uncertainty) + ((data - DN_fit)**2 / (2*(uncertainty**2)))) 
-
+    L = - (N/2)*jnp.log(2*math.pi) \
+        - jnp.sum(jnp.log(uncertainty) \
+        + ((data - DN_fit)**2 / (2*(uncertainty**2))))
     return L
 
 loglikelihood_jit = jax.jit(loglikelihood, static_argnums=[7])

maven_iuvs/graphics/echelle_graphics.py
@@ -1031,11 +1031,29 @@ def plot_line_fit(data_wavelengths, data_vals, model_fit, fit_params_for_printin
         if "failed_fit" in fit_params_for_printing:
             printme.append("Bad frame - no fit.")
         else:
-            if "maxLL" in fit_params_for_printing:
-                printme.append(f"Min chi sq.: {round(fit_params_for_printing['maxLL']) * 2}") # * 2 because we want to show chi squared
+            N = len(data_wavelengths)
+
+            if "maxLL" in fit_params_for_printing or "min_neg_LL" in fit_params_for_printing: 
+            # If either scipy or dynest was run,
+                if "maxLL" in fit_params_for_printing:
+                    maxLL = fit_params_for_printing['maxLL']
+
+                if "min_neg_LL" in fit_params_for_printing:
+                    maxLL = -1 * fit_params_for_printing['min_neg_LL']
+
+                printme.append(r"max $\ln\mathcal{L}$: "+f"{round(maxLL)}")
+
+                # Compute chisquared
+                chisq = round(-2 * (maxLL + (N*np.log(2*math.pi)/2) + np.sum(np.log(data_unc))))
+                reduced_chisq = round(chisq / (N-len(fit_params_for_printing.keys()) -1 ), 2)
+                printme.append(r"$\chi^2$: "+f"{chisq}")
+                printme.append(r"$\tilde{\chi}^2$: "+f"{reduced_chisq}")
+
             if "minchisq" in fit_params_for_printing: 
+                # Sometimes we plot IDL output, which reports min. chisq, which is secretly reduced chisq.
                 printme.append(r"$\tilde{\chi}^2$: " + f"{round(fit_params_for_printing['minchisq'], 2)}")
 
+            # Now do the stuff that should always be there....
             printme.append(f"H: {round(fit_params_for_printing['total_brightness_H'], 2)} ± {round(fit_params_for_printing['unc_total_brightness_H'], 2)} "+
                         f"kR (SNR: {round(fit_params_for_printing['total_brightness_H'] / fit_params_for_printing['unc_total_brightness_H'], 1)})")
             printme.append(f"D: {round(fit_params_for_printing['total_brightness_D'], 2)} ± {round(fit_params_for_printing['unc_total_brightness_D'], 2)} "+